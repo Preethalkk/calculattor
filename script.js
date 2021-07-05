859b07383a5ce2256c28c538d06af68dca9ebc32
Preethal <preethalpremraj1994@gmail.com>
2021-07-06 00:56:38 +0530

    calci

---
+++ script.js
@@ -0,0 +1,18 @@
+let displayNumber = (num) => inpt.value += num
+let evaluateExpression = () => {
+    if (inpt.value == "") {
+        inpt.value = 0
+    }
+    else {
+        try {
+            inpt.value = eval(inpt.value)
+        }
+        catch {
+            inpt.value = "error"
+        }
+    }
+}
+
+    let clearBox = () => inpt.value = " ";
+    let removeNumber = () => inpt.value = inpt.value.slice(0, -1);
+
